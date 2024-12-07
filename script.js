f9e46abaebe830b51529b0246683eaabd846d350
Ziaulhaq Parsa Karimi <154730860+Z-Coding-dot@users.noreply.github.com>
2024-12-07 21:33:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Bro :) Error😏";
+    }
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+function clearDisplayOne(){
+    display.value = display.value.toString().slice(0,-1);
+}
